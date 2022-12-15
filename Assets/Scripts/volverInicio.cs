f5b29169303cf10a9a2bbed835ee75cd43fc2688
Valeria Chinchilla <vchinchilla02@hotmail.com>
2022-12-15 15:35:21 -0600

    agregar gane

---
+++ Assets/Scripts/volverInicio.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class volverInicio : MonoBehaviour
+{
+    public void volver(){
+        SceneManager.LoadScene(0);
+    }
+}
